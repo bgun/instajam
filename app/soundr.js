40c08cb9220497534d7d1079e27d7d979daa43fb
Pavel Shub <pavel.987@gmail.com>
2013-10-19 00:59:31 -0400

    more simple code, need to make it not suck

--- app/soundr.js
+++ app/soundr.js
@@ -6,7 +6,21 @@ App.soundr = {
 	},
 	
 	aString: function() {
-		a = new Gibberish.KarplusStrong({ damping:.6 }).connect(); 
+		var a = new Gibberish.KarplusStrong({ damping:.6 }).connect(); 
 		a.note(440);
+	},
+	
+	startBeat: function() {
+		App.soundr.beatSound = new Gibberish.Snare({ snappy: 1.5 }).connect();
+	    App.soundr.beat = new Gibberish.Sequencer({
+	      target:App.soundr.beatSound, key:'note',
+	      values:[Gibberish.Rndf(-.05,.05)],
+	      durations:[ beats( 2 ) ]
+	    }).start();
+	},
+	
+	stopBeat: function() {
+		App.soundr.beat.stop();
+		App.soundr.beatSound.disconnect();
 	}
 };
