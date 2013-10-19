d47d38f865ba0cfdf3f304d72d3846ded2a1e4ae
Ben Gundersen <ben@bengundersen.com>
2013-10-18 21:47:31 -0400

    router

--- app/main.js
+++ app/main.js
@@ -1,2 +1,28 @@
+var App = {
+  Collections: {},
+  Models: {},
+  Views: {}
+}
+
 $(function() {
+
+  window.app = {};
+
+  Backbone.Router.extend({
+
+  routes: {
+    "/songs/:song/"          : "song",
+    "/songs/:song/conductor" : "conductor",
+    "/songs/:song/track"     : "track"
+  },
+
+  conductor: function(songId) {
+    app.views.conductor = new App.Views.ConductorView({
+      foo: "bar"
+    });
+  },
+
+  song: function(songId) {
+  }
+
 });
