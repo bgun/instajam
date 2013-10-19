25db91dbae31f4e6c51883a3ea82f5c70bfca4f7
Pavel Shub <pavel.987@gmail.com>
2013-10-19 02:30:27 -0400

    should make some noise

--- app/soundr.js
+++ app/soundr.js
@@ -19,19 +19,32 @@ App.soundr = {
 		for(var i=0;i<App.soundr.trackSetup.length;i++){
 			App.soundr.trackSetup[i].disconnect();
 		}
+		App.soundr.trackSetup = [];
 	},
 	
 	presets: {
 		base: function(){
-			App.soundr.trackSetup[0] = new Gibberish.KarplusStrong({ damping:.6 }).connect();
-			App.soundr.trackSetup[1] = new Gibberish.KarplusStrong({ damping:.6 }).connect();
-			App.soundr.trackSetup[2] = new Gibberish.KarplusStrong({ damping:.6 }).connect();
-			App.soundr.trackSetup[3] = new Gibberish.KarplusStrong({ damping:.6 }).connect();
+			App.soundr.trackSetup[0] = new Gibberish.PolySynth2({ attack:44800, decay:200, maxVoices:8 }).connect(); 
 			
-			App.soundr.tracks[0] = function(){App.soundr.trackSetup[0].note(440);};
-			App.soundr.tracks[1] = function(){App.soundr.trackSetup[1].note(880);};
-			App.soundr.tracks[2] = function(){App.soundr.trackSetup[2].note(1320);};
-			App.soundr.tracks[3] = function(){App.soundr.trackSetup[3].note(1760);};
+			/*App.soundr.trackSetup[12] = new Gibberish.Kick({}).connect();
+			App.soundr.trackSetup[13] = new Gibberish.Snare({}).connect();
+			App.soundr.trackSetup[14] = new Gibberish.Hat({}).connect();
+			App.soundr.trackSetup[15] = new Gibberish.CowBell({}).connect();*/
+			
+			App.soundr.tracks[0] = function(){App.soundr.trackSetup[0].note(110);};
+			App.soundr.tracks[1] = function(){App.soundr.trackSetup[0].note(220);};
+			App.soundr.tracks[2] = function(){App.soundr.trackSetup[0].note(440);};
+			App.soundr.tracks[3] = function(){App.soundr.trackSetup[0].note(880);};
+			App.soundr.tracks[4] = function(){App.soundr.trackSetup[0].note(1760);};
+			App.soundr.tracks[5] = function(){App.soundr.trackSetup[0].note(3520);};
+			App.soundr.tracks[6] = function(){/*App.soundr.trackSetup[0].note(100);*/};
+			App.soundr.tracks[7] = function(){/*App.soundr.trackSetup[0].note(100);*/};
+			
+			/*App.soundr.tracks[12] = function(){App.soundr.trackSetup[12].note();};
+			App.soundr.tracks[13] = function(){App.soundr.trackSetup[13].note();};
+			App.soundr.tracks[14] = function(){App.soundr.trackSetup[14].note();};
+			App.soundr.tracks[15] = function(){App.soundr.trackSetup[15].note();};*/
+			
 		}
 	},
 	
