eb40c70c781e027c56c97a4636e878e3891f94c1
Micah Freedman <i@micahfreedman.com>
2013-10-18 22:15:24 -0400

    updated collection endpoints

--- app/Track.js
+++ app/Track.js
@@ -1,8 +1,11 @@
 App.Models.TrackModel = Backbone.Model.extend({
+	defaults: {
+		frames: []
+	}
 
 });
 
 App.Collections.TrackCollection = Backbone.Firebase.Collection.extend({
 	model: App.Models.TrackModel,
-	firebase: new Backbone.Firebase("https://mobilejam.firebaseio.com")
+	firebase: new Backbone.Firebase("https://mobilejam.firebaseio.com/songs/tracks")
 });
