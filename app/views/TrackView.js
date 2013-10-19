5192e849573a77383b0c726100729f4a0b8b10e4
Ben Gundersen <ben@bengundersen.com>
2013-10-19 00:57:59 -0400

    balls

--- app/views/TrackView.js
+++ app/views/TrackView.js
@@ -4,7 +4,7 @@ App.Views.TrackView = Backbone.View.extend({
 
   tagName: "div",
 
-  template: "#track-view-template",
+  template: "track",
 
   events: {
   },
@@ -12,9 +12,20 @@ App.Views.TrackView = Backbone.View.extend({
   initialize: function() {
     var t = this;
     t.render(t.options.songId);
+
+    $.ajax({
+      url: "app/templates/track.tmpl.html",
+      async: true,
+      type: "GET",
+      dataType: "text",
+      success: function(resp) {
+        console.log(_.template(resp));
+      }
+    });
   },
 
   render: function(songId) {
+    var t = this;
     console.log(songId);
   }
 
