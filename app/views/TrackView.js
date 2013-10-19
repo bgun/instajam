86a8275dbbbd7c96bee9a049d9a12ca870376585
Ben Gundersen <ben@bengundersen.com>
2013-10-19 02:16:17 -0400

    YES

--- app/views/TrackView.js
+++ app/views/TrackView.js
@@ -16,19 +16,36 @@ App.Views.TrackView = Backbone.View.extend({
 
   render: function(songId) {
     var t = this;
+    var i,j;
     console.log(songId);
 
     var _tmpl = _.template(templateManager.getTemplate(t.template));
 
-    var grid = Array(12);
-    for(var i=0;i<grid.length;i++) {
-      grid[i] = Array(12);
+    var gridSize = 16;
+    var grid = Array(gridSize);
+    var index = 0;
+    for(i=0;i<grid.length;i++) {
+      grid[i] = Array(gridSize);
+      for(j=0;j<grid[i].length;j++) {
+        grid[i][j] = {
+          index: index
+        }
+        index++;
+      }
     }
-    var html = _tmpl({
-      grid: grid
-    });
 
-    $('#content').html(html);
+    t.$el.html(_tmpl({
+      grid: grid
+    }));
+
+    console.log(t.$el);
+
+    $('#content').html(t.$el);
+
+    $('#content').on('click','.button',function(e) {
+      e.preventDefault();
+      $(this).toggleClass('active');
+    });
   }
 
 });
