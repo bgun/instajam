b239c447e55114a9a00c66ca19b804c4b5822237
Micah Freedman <i@micahfreedman.com>
2013-10-19 01:20:10 -0400

    templateManager exists

---
+++ app/templateManager.js
@@ -0,0 +1,27 @@
+var templateManager = (function($, _) {
+  var templates = {},
+      templateKeys = ['song', 'track'];
+
+  var getTemplate = function(key) {
+    if (!templates[key]) {
+      $.ajax({
+        async: false,
+        url: 'app/templates/' + key + '.ejs.html', 
+        success: function(data){
+          templates[key] = data;
+        }
+      });
+    }
+    return templates[key];
+  };
+
+  var loadTemplates = function() {
+    _(templateKeys).each(getTemplate);
+  };
+
+  return {
+    getTemplate: getTemplate,
+    loadTemplates: loadTemplates,
+    templates: templates
+  };
+})(jQuery, _);
